e8eb6bf729dc22fd0243e691ffc043961d9d0377
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2022-03-26 00:01:42 +0530

    minor changes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const multer = require("multer");
 
 const checkAuth = require('../middleware/checkAuth');
 const UsersControllers = require('../controllers/users');
-const upload = multer({ dest: "media/" });
 
 
-// router.get('/validate', checkAuth, UsersControllers.validate);
 router.get('/hash', checkAuth, UsersControllers.get_hash);
 router.post('/signup', UsersControllers.signup_user);
 router.get('/:_id', checkAuth, UsersControllers.get_user);
 router.delete('/:_id', checkAuth, UsersControllers.delete_user);
 router.put('/:_id', checkAuth, UsersControllers.update_user);
-// router.post('/login',UsersControllers.login_user );
 
 module.exports = router;
